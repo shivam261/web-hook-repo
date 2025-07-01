9f4806429878239b86fdffca9498cf46c372ee03
shivam <shivam1705of@gmail.com>
2025-07-01 15:47:18 +0530

    fix: production  setup

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn --factory -w 4 -b 0.0.0.0:$PORT run:create_app
--- run.py
+++ run.py
@@ -4,6 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", 5000))
-    app.run(host=host, port=port, debug=True)
+    h=os.getenv("HOST","127.0.0.1" )
+    app.run(host=h,debug=False)
