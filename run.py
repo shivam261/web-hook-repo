d4a51d4873180644819735dd195c44d8a80890e2
shivam <shivam1705of@gmail.com>
2025-07-01 15:43:41 +0530

    fix: production gunicorn setup

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 from app import create_app
 import os
+
 app = create_app()
 
 if __name__ == '__main__':
-    
-    h=os.getenv("HOST","127.0.0.1" )
-    if(os.getenv("FLASK_ENV")=="development"):
-        app.run(host=h,debug=True)
-    else:
-        app.run(host=h,debug=True)
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT", 5000))
+    app.run(host=host, port=port, debug=True)
